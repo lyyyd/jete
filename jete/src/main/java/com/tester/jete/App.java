328e6768871f59cc2e50b5d2f2b293401547d1ec
OrangeManDavid <OrangeHeroDavid@163.com>
2020-04-30 13:15:53 +0800

    Hello Jete！ The World exists, whether you like or not.

---
+++ jete/src/main/java/com/tester/jete/App.java
@@ -0,0 +1,13 @@
+package com.tester.jete;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jete!" );
+    }
+}
