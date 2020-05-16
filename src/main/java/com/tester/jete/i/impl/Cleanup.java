c3d2a60d3bacbdeeda5e449e74b634cac65e07a7
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-05-16 17:59:25 +0800

    Cleanup

---
+++ src/main/java/com/tester/jete/i/impl/Cleanup.java
@@ -0,0 +1,53 @@
+package com.tester.jete.i.impl;
+
+import org.openqa.selenium.InvalidSelectorException;
+
+/**
+ * @ClassName Cleanup
+ * @Description TODO
+ * @Author David.Jackson.Lyd@gmail.com
+ * @Date: 2020/05/16 17:58
+ */
+public class Cleanup {
+    public static Cleanup of = new Cleanup();
+
+    public String webdriverExceptionMessage(Throwable webDriverException) {
+        return webdriverExceptionMessage(webDriverException.toString());
+    }
+
+    public String webdriverExceptionMessage(String webDriverExceptionInfo) {
+        return webDriverExceptionInfo == null || webDriverExceptionInfo.indexOf('\n') == -1 ?
+                webDriverExceptionInfo :
+                webDriverExceptionInfo
+                        .substring(0, webDriverExceptionInfo.indexOf('\n'))
+                        .replaceFirst("(.*)\\(WARNING: The server did not provide any stacktrace.*", "$1")
+                        .replaceFirst("org\\.openqa\\.selenium\\.(.*)", "$1")
+                        .trim();
+    }
+
+    public boolean isInvalidSelectorError(Throwable error) {
+        if (error == null) return false;
+
+        String message = error.getMessage();
+        if (message == null) return false;
+
+        return (error instanceof InvalidSelectorException && !message.contains("\"Element is not selectable\"")) ||
+                isInvalidSelectorMessage(message) ||
+                error.getCause() != null && error.getCause() != error && isInvalidSelectorError(error.getCause());
+    }
+
+    private boolean isInvalidSelectorMessage(String message) {
+        return message.contains("invalid or illegal string was specified") ||
+                message.contains("Invalid selector") ||
+                message.contains("invalid selector") ||
+                message.contains("is not a valid selector") ||
+                message.contains("SYNTAX_ERR") ||
+                message.contains("INVALID_EXPRESSION_ERR");
+    }
+
+    public InvalidSelectorException wrap(Throwable error) {
+        return (error instanceof InvalidSelectorException) ?
+                (InvalidSelectorException) error :
+                new InvalidSelectorException("Invalid selector", error);
+    }
+}
