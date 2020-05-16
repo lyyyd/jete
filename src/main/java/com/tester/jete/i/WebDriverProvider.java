6599ea2b6eea223c9e6cc6f1dfc53adbe81b1549
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-05-16 17:43:45 +0800

    add interface WebDriverProvider

---
+++ src/main/java/com/tester/jete/i/WebDriverProvider.java
@@ -0,0 +1,34 @@
+package com.tester.jete.i;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * <p>
+ *   Interface for using custom WebDriver in your tests
+ * </p>
+ *
+ * <p>
+ * To customize {@link WebDriver} creation one can use any of the alternatives:
+ * <ul>
+ * <li>Call method {@link com.codeborne.selenide.WebDriverRunner#setWebDriver(WebDriver)} explicitly.
+ * <li>Extend {@link WebDriver} implementation, override {@code public XxxDriver(Capabilities desiredCapabilities)}
+ * constructor and pass this class name as {@code browser} system variable value.
+ * <li>Implement this very interface and pass the implementation class name as {@code browser} system variable value.
+ * </ul>
+ */
+public interface WebDriverProvider {
+
+    /**
+     * Create new {@link WebDriver} instance. The instance will be bound to current thread, so there is no need to cache
+     * this instance in method implementation. Also don't cache the instance in static variable, as <a
+     * href="http://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Is_WebDriver_thread-safe?">WebDriver
+     * instance is not thread-safe</a>.
+     *
+     * @param desiredCapabilities set of desired capabilities as suggested by Selenide framework; method implementation is
+     * recommended to pass this variable to {@link WebDriver}, probably modifying it according to specific needs
+     * @return new {@link WebDriver} instance
+     */
+    WebDriver createDriver(DesiredCapabilities desiredCapabilities);
+}
+
