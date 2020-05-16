08b892fc72927759c9d00a79118755925ada97d7
OrangeManDavid <david.jackson.lyd@gmail.com>
2020-05-16 18:17:18 +0800

    DownloadFileWithProxyServer

---
+++ src/main/java/com/tester/jete/i/impl/DownloadFileWithProxyServer.java
@@ -0,0 +1,10 @@
+package com.tester.jete.i.impl;
+
+/**
+ * @ClassName DownloadFileWithProxyServer
+ * @Description TODO
+ * @Author David.Jackson.Lyd@gmail.com
+ * @Date: 2020/05/16 18:16
+ */
+public class DownloadFileWithProxyServer {
+}
